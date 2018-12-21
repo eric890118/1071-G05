451959beafec3557bbfafdff9ed1dccefc0f165b
eric890118 <43335682+eric890118@users.noreply.github.com>
2018-12-21 22:36:07 +0800

    Update main.cpp

--- project/main.cpp
+++ project/main.cpp
@@ -3,21 +3,12 @@
 #include <time.h>
 
 int main(){
-    int choice;
+    srand(time(NULL));
     int award;
-    printf("");
-    
-    
-    
-    scanf("%d", &choice);
-    while(choice!=-1){
-        srand(time(NULL));
-        int num = rand()%26+65;
-        int num2 = rand()%26+65;
-        printf("%c%c - ", num, num2);
-        for(int i=1 ; i<=8 ; i++){
-            int eng = rand()%10;
-            printf("%d",eng);
-        }
-    }
+    int num;
+    invoice(award);
+    inv(num);
+    check(award);
+    money(award);
+
 }
