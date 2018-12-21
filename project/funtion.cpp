7c35be1f29396e3facb01d9c001eeb5faa6d97cb
eric890118 <43335682+eric890118@users.noreply.github.com>
2018-12-21 23:11:47 +0800

    Create funtion.cpp

---
+++ project/funtion.cpp
@@ -0,0 +1,30 @@
+void invoice(int award){
+    int choice;
+    while(1){
+        printf("choice :");
+        scanf("%d", &choice);
+        if(choice = 1){}
+
+        int eng1 = rand()%26+65;
+        int eng2 = rand()%26+65;
+        printf("%c%c - ", eng1, eng2);
+        for(int i=1 ; i<=8 ; i++){
+            int num = rand()%10;
+            printf("%d",num);
+        }
+        printf("\nchoice: ");
+    	scanf("%d", &choice);
+    }
+}
+
+void inv(int num){
+
+}
+
+void check(int award){
+
+}
+
+void money(int award){
+
+}
