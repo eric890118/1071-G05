d496d29640d150084799790c653fb2a54d3f5edf
eric890118 <43335682+eric890118@users.noreply.github.com>
2018-12-25 15:30:34 +0800

    Update main.cpp

--- project/main.cpp
+++ project/main.cpp
@@ -5,10 +5,11 @@
 int main(){
     srand(time(NULL));
     int award;
-    int num;
-    invoice(award);
-    inv(num);
-    check(award);
-    money(award);
-
+    int prizeN[45];          //存中獎號碼
+    int n;                   //輸入幾張發票 最多100張
+    int ownN[MAX];           //存亂數產生的發票 1~10 EE-NNNNNNNN
+    invoice(prizeN);         //產生中獎號碼
+    owninv(n,ownN);          //輸入有幾張發票
+    check(prizeN,ownN);      //對獎
+    money(prizeN,ownN);      //中獎金額/細項
 }
