e4896ca209fceec4e374763cbc59bf3164260868
eric890118 <43335682+eric890118@users.noreply.github.com>
2018-12-25 14:38:13 +0800

    Update funtion.cpp

--- project/funtion.cpp
+++ project/funtion.cpp
@@ -1,27 +1,36 @@
-void invoice(int award){
+void invoice(int prizeN[]){
     int choice;
-    while(1){
-        printf("choice :");
-        scanf("%d", &choice);
-        if(choice = 1){}
+    printf("1. Show the prize number\n");
+    printf("2. Needn't show and go to next\n");
+    printf("=> ");
+    scanf("%d", &choice);
+    for(int a=1;a<=5;a++){
 
-        int eng1 = rand()%26+65;
-        int eng2 = rand()%26+65;
-        printf("%c%c - ", eng1, eng2);
         for(int i=1 ; i<=8 ; i++){
             int num = rand()%10;
-            printf("%d",num);
+            if(choice == 1) printf("%d",num);
         }
-        printf("\nchoice: ");
-    	scanf("%d", &choice);
+        if(a==1) printf(" <Special award>(10 million)");
+        if(a==2) printf(" <Special award>(2 million)");
+        if(a==3) printf(" <Prize Number>");
+        if(a==4) printf(" <Prize Number>");
+        if(a==5) printf(" <Prize Number>");
+        if(choice == 1) printf("\n");
+    }
+    for(int a=1;a<=2;a++){
+        for(int i=1 ; i<=3 ; i++){
+            int num = rand()%10;
+            if(choice == 1) printf("%d",num);
+        }
+        if(choice == 1) printf(" <Last 3 Number>\n");
     }
 }
 
-void inv(int num){
+void inv(int n){
 
 }
 
-void check(int award){
+void check(int ){
 
 }
 
