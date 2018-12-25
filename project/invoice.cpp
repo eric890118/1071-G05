7649b6db5c877ecefb1eda9506d81b4b5590667d
firedarktoby <43335745+firedarktoby@users.noreply.github.com>
2018-12-26 04:10:20 +0800

    Update invoice.cpp

--- project/invoice.cpp
+++ project/invoice.cpp
@@ -6,6 +6,11 @@ void invoice(int award[]){
 	int temp = 0;
 	int a = 0;
 	int b = 0;
+	int choice;
+	printf("1. Show the prize number.\n");
+	printf("2. Needn't show and go to next.\n");
+	printf("--> ");
+	scanf("%d", &choice);
 	for(int c=1; c<=5; c++){
 		temp = 0;
 		for(int i=1, k=1; i<=8; i++, k*=10){
@@ -27,36 +32,94 @@ void invoice(int award[]){
 			award[b++] = temp;
 		}
 	}
-	for(int j=0;j<5;j++){
-		printf("%08d\n", award[j]);
+	if(choice==1){
+		for(int j=0;j<5;j++){
+			printf("%08d", award[j]);
+			if(j==0)printf("    <Special award>(10 million)\n");
+			if(j==1)printf("    <Special award>(2 million)\n");
+			if(j==2)printf("    <Prize Number>\n");
+			if(j==3)printf("    <Prize Number>\n");
+			if(j==4)printf("    <Prize Number>\n");
+		}
+		for(int j=5;j<8;j++){
+			printf("%03d", award[j]);
+			printf("         <Last 3 Number>\n");
+		}
 	}
-	for(int j=5;j<8;j++){
-		printf("%03d\n", award[j]);
+}
+
+void owninv(int own[],int n){
+	int b = 0;
+	int a = 0;
+	int temp = 0;
+	int choice;
+	printf("1. Show your invoice.\n");
+	printf("2. Needn't show and go to next.\n");
+	printf("--> ");
+	scanf("%d", &choice);
+
+	for(int c=1; c<=n; c++){
+		temp = 0;
+		for(int i=1, k=1; i<=8; i++, k*=10){
+			a = ((rand()%10)*10*k)/(10);
+			temp = a + temp;
+			//printf(" a = %d \n",a);
+			if(i==8)
+				own[b++] = temp;
+		}
 	}
+	if(choice==1){
+		for(int j=0;j<n;j++){
+			printf("%08d\n", own[j]);
+		}
+	}
+}
+
+void check(int award[],int own[],int n){
+	int aw = 0;
+	for(int j=0; j<2; j++){			//specical
+		for(int i=0;i<n;i++){
+		if(award[j]==own[i])aw++;
+		}
+	}
+	for(int j=2; j<5; j++){			//prize
+		for(int i=0;i<n;i++){
+			for(int k=1; k<=100000; k*=10){
+				if(award[j]%(1000*k)==own[i]%(1000*k)){
+					printf("pr = %08d\n",own[i]);	
+				}
+			}
+		}
+	}
+	for(int j=5; j<8; j++){			//last 3 number
+		for(int i=0;i<n;i++){
+			if(award[j]==own[i]%1000){
+				printf("%08d\n",own[i]);
+				aw++;	
+			}
+		}
+	}
+	printf("%d\n",aw);
 }
 
 int main(){
     srand(time(NULL));
     int N;
     int num;
-	int choice;
 	int award[8];
+	int own[1000];
 	
-	while(1){
-		printf("1. 9~10 month award: \n");
-		printf("2. exit\n");
-		printf("--> ");
-    	scanf("%d", &choice);
-    	if(choice==2)break;
-		switch(choice){
-			case 1:
-				invoice(award);
-				printf("\n");	
-				break;
-		}	
-	}
+	invoice(award);
+	printf("\n");
+    printf("Input the number of invoice sheets n(Max input 1000):  ");
+    scanf("%d", &N);
+	owninv(own,N);			
     //inv(num);
-    //check(award);
-    //money(award);
+	check(award,own,N);
+	//money(award);
 }
+
+		
+		
+
 
